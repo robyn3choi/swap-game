02c948190400384894ef9bf3914ed14cbc4fd9bd
Robyn Choi <robyn3choi@gmail.com>
2017-03-18 16:48:57 -0700

    timer countdown ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     int playerOneScore = 0;
     int playerTwoScore = 0;
     int timeLeft = 120;
+    string formattedTime = "2:00";
 
     bool gameOver;
 
     string endGameString;
+
+    public Text timer;
 
 	// Use this for initialization
 	void Start () {
@@ -29,20 +33,19 @@ public class GameManager : MonoBehaviour {
     void Count() {
         if (timeLeft > 0) {
             timeLeft--;
-            print(timeLeft);
+            FormatTime();
+            timer.text = formattedTime;
         }
         else {
             EndGame();
         }
     }
 
-//    void OnGUI() {
-//        int minutes = Mathf.FloorToInt(timeLeft / 60F);
-//        int seconds = Mathf.FloorToInt(timeLeft - minutes * 60);
-//        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
-//
-//        GUI.Label(new Rect(10,10,250,100), niceTime);
-//    }
+    void FormatTime() {
+        int minutes = Mathf.FloorToInt(timeLeft / 60F);
+        int seconds = Mathf.FloorToInt(timeLeft - minutes * 60);
+        formattedTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+    }
 
     void EndGame() {
         gameOver = true;
