b2358dce7f9b377dbff0bfffe88bba02d1732ae3
Tony Lee <tonyslee0693@gmail.com>
2017-03-18 18:28:53 -0700

    Added enemy AI movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed;
+    public Transform Target;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector2.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+           
+    }
+}
