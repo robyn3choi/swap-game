eedb8413f63ab04bf64bcbfcced723dedf69eab9
j-lou <johnny.lou8@gmail.com>
2017-03-18 18:01:35 -0700

    Adding player health

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private int score;
+	private int healthPoints = 1;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,14 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void LoseHealth (int lossHealth) {
+		// Set animation of player being hit
+		healthPoint -= lossHealth;
+		if (healthPoint <= 0) {
+			// kill the player
+		}
 	}
 
     public int GetScore() {
